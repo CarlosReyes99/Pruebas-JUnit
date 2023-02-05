9e8321110525a621e40a7813283c7f0b942dd295
Carlos Reyes <carlosmxm055@gmail.com>
2023-02-04 18:06:48 -0800

    Organizando los test

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -2,6 +2,9 @@ public class StringUtil {
 
     public static String repeat(String msj, int num){
         StringBuilder result= new StringBuilder();
+        if(num<0){
+            throw new IllegalArgumentException("negative times not allowed");
+        }
         for (int i = 0; i < num; i++) {
             result.append(msj);
         }
